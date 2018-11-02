9597888ffc8dfcd0c5627ff47e077159d6df4099
HolaSoyLuis <luis95velasquez@outlook.com>
2018-11-01 21:13:22 -0600

    improvements on Asignacion

--- resources/views/asignacion/create.blade.php
+++ resources/views/asignacion/create.blade.php
@@ -37,39 +37,63 @@
     @endif
     <div class="container">
       <div class="row justify-content-center">
-        <div class="col-md-4">
-          <div class="card">
             <div class="card-body text-center">
       <form method="post" action="{{ route('asignacion.store') }}">
         <h3>Ingrese los Datos</h3>
+        <div class="col">
+          <div class="form-group">
           <div class="form-group label-floating">
               {{ csrf_field() }}
               <label for="fecha_asignacion">Fecha de asignacion: </label>
               <input type="date" class="form-control" name="fecha_asignacion"/>
           </div>
+          </div>
+          </div>
+          <div class="col">
+          <div class="form-group">
           <div class="form-group label-floating">
+            <!--
               <label for="nuevo_reingreso">Nuevo/Reingreso: </label>
               <input type="text" class="form-control" name="nuevo_reingreso"/>
+            -->
+            <label for="nuevo_reingreso">Seleccione nuevo/reingreso</label>
+            <select class="form-control" name="nuevo_reingreso" id="nuevo_reingreso">
+              <option value="1">Nuevo</option>
+              <option value="2">Reingreso</option>
+            </select>
           </div>
+          </div>
+          </div>
+          <div class="col">
+          <div class="form-group">
           <div class="form-group label-floating">
               <label for="certificado">Certificado: </label>
               <input type="text" class="form-control" name="certificado"/>
           </div>
+          </div>
+          </div>
+          <div class="col">
+          <div class="form-group">
           <div class="form-group label-floating">
               <label for="clave_estudiante">Clave del estudiante: </label>
               <input type="text" class="form-control" name="clave_estudiante"/>
           </div>
+          </div>
+          </div>
+          <div class="col">
+          <div class="form-group">
           <div class="form-group label-floating">
               <label for="estudiante_id">Estudiante</label>
               <select class="form-control" name="estudiante_id" id="estudiante_id">
                   @foreach ($estudiantes as $estudiante)
-                  <!--
-                    <option value="{{ $estudiante['id'] }}">{{ $estudiante['id'] }}</option>
-                  -->
                     <option value="{{ $estudiante['id'] }}">{{ $estudiante['p_nombre'] }} {{ $estudiante['s_nombre'] }} {{ $estudiante['p_apellido'] }} {{ $estudiante['s_apellido'] }}</option>
                   @endforeach
               </select>
           </div>
+          </div>
+          </div>
+          <div class="col">
+          <div class="form-group">
           <div class="form-group label-floating">
               <label for="ciclo_id">Ciclo</label>
               <select class="form-control" name="ciclo_id" id="ciclo_id">
@@ -78,6 +102,10 @@
                   @endforeach
               </select>
           </div>
+          </div>
+          </div>
+          <div class="col">
+          <div class="form-group">
           <div class="form-group label-floating">
               <label for="grado_id">Grado</label>
               <select class="form-control" name="grado_id" id="grado_id">
@@ -86,6 +114,10 @@
                   @endforeach
               </select>
           </div>
+          </div>
+          </div>
+          <div class="col">
+          <div class="form-group">
           <div class="form-group label-floating">
               <label for="seccion_id">Seccion</label>
               <select class="form-control" name="seccion_id" id="seccion_id">
@@ -93,6 +125,8 @@
                     <option value="{{ $seccion['id'] }}">{{ $seccion['seccion'] }} - {{ $seccion['grado_id'] }}</option>
                   @endforeach
               </select>
+          </div>
+          </div>
           </div>
           <button type="submit" class="btn btn-primary">Agregar</button>
       </form>
