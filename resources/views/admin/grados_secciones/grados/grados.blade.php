da04aff00550edbdd9ce8d50263214be947bc307
HolaSoyLuis <luis95velasquez@outlook.com>
2018-11-18 17:51:03 -0600

    cambios varios

--- resources/views/admin/grados_secciones/grados/grados.blade.php
+++ resources/views/admin/grados_secciones/grados/grados.blade.php
@@ -2,9 +2,9 @@
 @section('title', 'Grados')
 @section('content')
 <div class="row">
-	<div class="col-lg-12 col-md-8 col-sm-8 col-xs-12">		
+	<div class="col-lg-12 col-md-8 col-sm-8 col-xs-12">
 		<nav class="navbar navbar-expand-lg bg-primary">
-		  <div class="container">		    		    		    
+		  <div class="container">
 		    <div class="collapse navbar-collapse">
 		      <ul class="navbar-nav">
 		        <li class="nav-item active">
@@ -31,20 +31,20 @@
 		<div class="table-responsive">
 			<table class="table table-striped table-bordered table-condensed table-hover">
 				<thead>
-					<th>No</th>						
+					<th>No</th>
 					<th>Jornada</th>
 					<th>Grado</th>
 					<th>Creado</th>
 					<th>Actualizado</th>
 					<th>Opciones</th>
-				</thead>		
+				</thead>
 				@foreach($grados as $grado)		
 				<tr>
 					<td>{{$grado->id}}</td>
-					<td>{{$grado->jornada->jornada}}</td>
+					<td>{{$grado->jornada}}</td>
 					<td>{{$grado->grado}}</td>
 					<td>{{$grado->created_at}}</td>
-					<td>{{$grado->updated_at}}</td>					
+					<td>{{$grado->updated_at}}</td>
 					<td>
 						<a href="#">
 							<button class="btn btn-info btn-sm" title="Ver">
@@ -65,10 +65,10 @@
 							</button>
 						</a>
 					</td>
-				</tr>	
-				@endforeach						
+				</tr>
+				@endforeach
 			</table>
-		</div>		
+		</div>
 	</div>
 </div>
 @endsection
